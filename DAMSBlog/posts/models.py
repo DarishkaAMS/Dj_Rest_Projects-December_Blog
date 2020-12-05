a2d9c25b2d9891abc323af6dd03466813e648943
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-05 08:55:09 +0200

    add class Post

--- DAMSBlog/posts/models.py
+++ DAMSBlog/posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+# MVC - Model View Controller
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __init__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
