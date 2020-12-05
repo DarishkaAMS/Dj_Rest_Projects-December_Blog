ff2f233cf8c2a643832ae310f46c6acf61ace2b0
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-05 10:05:26 +0200

    Add post_views mapping

--- DAMSBlog/posts/urls.py
+++ DAMSBlog/posts/urls.py
@@ -3,9 +3,9 @@ from .views import posts_create, posts_update, posts_delete, posts_detail, posts
 
 
 urlpatterns = [
+    path('', posts_list, name='posts_list'),
     path('create/', posts_create, name='posts_create'),
     path('detail/', posts_detail, name='posts_detail'),
-    path('list/', posts_list, name='posts_list'),
     path('update/', posts_update, name='posts_update'),
     path('delete/', posts_delete, name='posts_delete'),
 ]
