89f689b0dfbc9ce50e443a5a20911b6de46882f4
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-05 08:35:33 +0200

    Initial Commit

--- DAMSBlog/DAMSBlog/urls.py
+++ DAMSBlog/DAMSBlog/urls.py
@@ -1,4 +1,4 @@
-"""December_Blog URL Configuration
+"""DAMSBlog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- DAMSBlog/DAMSBlog/wsgi.py
+++ DAMSBlog/DAMSBlog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for December_Blog project.
+WSGI config for DAMSBlog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'December_Blog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DAMSBlog.settings')
 
 application = get_wsgi_application()
