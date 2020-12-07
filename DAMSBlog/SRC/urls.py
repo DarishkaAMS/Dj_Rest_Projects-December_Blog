8ecdae1cf676c176dcb4c7b4c049495e849bca27
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-12-07 05:36:13 +0200

    Setting STATIC

--- DAMSBlog/SRC/urls.py
+++ DAMSBlog/SRC/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from posts import views as post_views
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls'), name='posts_urls'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
